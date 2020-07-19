c08594c7869e87668eeebfd0cb38a1dd3467e3aa
maher elsayed <maher.elsayed.dev@gmail.com>
2020-07-19 07:53:03 +0200

    feat: Add, Delete and complete todo

--- src/components/todo-form.vue
+++ src/components/todo-form.vue
@@ -1,13 +1,30 @@
 <template lang="html">
-  <v-text-field
-    label="Todo"
-    placeholder="What do you have to do?"
-    solo
-  />
+  <form @submit.prevent="addTodo">
+    <v-text-field
+      label="Todo"
+      placeholder="What do you have to do?"
+      solo
+      v-model="task"
+      required
+    />
+  </form>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      task: "",
+      newId: 0
+    };
+  },
+  methods: {
+    addTodo: function() {
+      this.$store.dispatch("addTodo", this);
+      this.newId++;
+      this.task = "";
+    }
+  }
 }
 </script>
 
--- src/components/todo-list.vue
+++ src/components/todo-list.vue
@@ -1,20 +1,52 @@
 <template lang="html">
   <div>
-    <TodoItem class="mb-3" />
+    <v-card 
+      v-for="todo in todos"
+      :key="todo.id"
+      :class="{completed: todo.completed}"
+      class="task"
+      @click="toggleTodo(todo.id)"
+    >
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline mb-0">{{ todo.task }}</h3>
+        </div>
+      </v-card-title>
+      <v-btn
+        @click="toggleTodo(todo.id)"
+      >
+        Complete
+      </v-btn>
+      <v-btn 
+        color="error" 
+        @click="deleteTodo(todo.id)"
+      >
+        Delete
+      </v-btn>
+    </v-card>
   </div>
 </template>
 
 <script>
-import TodoItem from '@/components/todo-item';
-
-
 export default {
-  name: 'Todos',
-  components: {
-    TodoItem,
+  computed: {
+    todos() {
+      return this.$store.getters.getTodos;
+    }
+  },
+  methods: {
+    toggleTodo: function(id) {
+      this.$store.dispatch("toggleTodo", id);
+    },
+    deleteTodo: function(id) {
+      this.$store.dispatch("deleteTodo", id);
+    },
   }
 }
 </script>
 
 <style lang="css">
+.completed h3 {
+  text-decoration: line-through;
+}
 </style>
